ca97056b6c6abeeecc6a72997295edd4d2c8a82a
daxab <93872444+daxab@users.noreply.github.com>
2021-11-07 18:13:33 +0300

    Add files via upload

---
+++ encrypter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing.Design;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+public class Encrypter
+{
+	public static string ApplyXOR(string message, string key)
+	{
+		int ch;
+		int n;
+		int int_key = 0;
+		string result = "";
+		for( int i = 0; i < key.Length; i ++)
+		{
+			int_key += Convert.ToInt32 ((key).ToCharArray()[i]);
+
+		}
+
+
+		for(int i = 0; i < message.Length; i++)
+		{
+			ch = (int) message[i];
+			n = ch^int_key;
+			result += (char)n;
+		}
+		return result;
+	}
+
+}
+
+
